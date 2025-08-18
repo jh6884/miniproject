072ef6e1cafd77c0e70d5553736e3237fb5d0a9b
shinjaehyeon <pumpkinalpha996@gmail.com>
2025-08-18 14:33:34 +0900

    api 사용해보기

--- src/tests/crosswalk_light.py
+++ src/tests/crosswalk_light.py
@@ -1,15 +1,29 @@
 # 핵심 기능: 횡단보도 신호를 파악하기
 # 목표: 객체 인식을 통해 보행 신호등을 검출하기
 
-from ultralytics import YOLO
+from inference import get_model
+import supervision as sv
 import cv2
-import matplotlib.pyplot as plt
 
+img_path = "../../data/test03.jpg"
+image = cv2.imread(img_path)
 
-model = YOLO("yolo11n.pt")
+model = get_model(model_id='traffic-light-detection-h8cvg/2')
 
-img = '../../data/test09.jpg'
+results = model.infer(image)
 
-# results = model(img)
+# load the results into the supervision Detections api
+detections = sv.Detections.from_inference(results[0].dict(by_alias=True, exclude_none=True))
 
-train_model = model.train(data="../../data/dataset/data.yaml", epochs = 5, imgsz = 640)
+# create supervision annotators
+bounding_box_annotator = sv.BoxAnnotator()
+label_annotator = sv.LabelAnnotator()
+
+# annotate the image with our inference results
+annotated_image = bounding_box_annotator.annotate(
+    scene=image, detections=detections)
+annotated_image = label_annotator.annotate(
+    scene=annotated_image, detections=detections)
+
+# display the image
+sv.plot_image(annotated_image)
