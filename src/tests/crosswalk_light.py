c7e5fd5c47f67b09f5e2da7fcb2ac20a54d53586
shinjaehyeon <pumpkinalpha996@gmail.com>
2025-08-18 10:36:40 +0900

    횡단보도 신호등 검출

--- src/tests/crosswalk_light.py
+++ src/tests/crosswalk_light.py
@@ -1,4 +1,14 @@
 # 핵심 기능: 횡단보도 신호를 파악하기
+# 목표: 객체 인식을 통해 보행 신호등을 검출하기
 
 from ultralytics import YOLO
+import cv2
+import matplotlib.pyplot as plt
 
+model = YOLO("yolo11n.pt")
+
+img = '../../data/test09.jpg'
+
+results = model(img)
+
+results[0].show()
