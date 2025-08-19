d34f59f1555428c4ea8a8dfb6afec2bd96fd931e
shinjaehyeon <pumpkinalpha996@gmail.com>
2025-08-19 13:21:24 +0900

    update

--- src/tests/temp.py
+++ src/tests/temp.py
@@ -3,10 +3,16 @@ import os, cv2
 import matplotlib.pyplot as plt
 
 model = YOLO('runs/detect/train/weights/best.pt')
+model2 = YOLO('yolo11n.pt')
 
-img_path = '../../data/images/06.jpg'
+img_path = '../../data/images/'
+lst = os.listdir(img_path)
 
+result = model(img_path+'09.jpg', conf=0.1)
+result2 = model2(img_path+'09.jpg', conf=0.1)
+# for file in lst:
+#     results = model(img_path+file, conf=0.1)
 
-result = model(img_path, conf=0.1)
 
 result[0].show()
+result2[0].show()
