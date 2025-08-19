03485504e26588a5642092e19c0d298a2259b526
shinjaehyeon <pumpkinalpha996@gmail.com>
2025-08-19 16:34:13 +0900

    keep testing

--- src/tests/temp.py
+++ src/tests/temp.py
@@ -4,23 +4,24 @@ import matplotlib.pyplot as plt
 from inference_sdk import InferenceHTTPClient
 
 model = YOLO('runs/detect/train/weights/best.pt')
-model2 = YOLO('yolo11n.pt')
+model2 = YOLO('yolo11s.pt')
 
 img_path = '../../data/images/'
-img_name = '03.jpg'
+img_name = '06.jpg'
 lst = os.listdir(img_path)
 img = cv2.imread(img_path+img_name)
-# result = model(img_path+img_name, conf=0.1)
-# result2 = model2(img_path+img_name, conf=0.3)
-# for file in lst:
-#     results = model(img_path+file, conf=0.1)
+result = model(img_path+img_name, conf=0.1)
+result2 = model2(img_path+img_name, conf=0.3)
+
+result[0].show()
+result2[0].show()
 
 CLIENT = InferenceHTTPClient(
     api_url="https://serverless.roboflow.com",
     api_key="Ct4LGBM8nYfY0kBogGnU"
 )
 
-result3 = CLIENT.infer(img_path+img_name, model_id="traffic-light-v9orl/3")
+result3 = CLIENT.infer(img_path+img_name, model_id="traffic-light-vzfpm/3")
 
 print(result3)
 
@@ -41,6 +42,5 @@ for i in range(len(pred)):
     cv2.rectangle(img, (xtl, ytl), (xbr, ybr), color, thickness=1, lineType=cv2.LINE_AA)
     cv2.putText(img, pred[i]['class'], (xtl, ytl-10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, color, 1)
     cv2.imshow('image', img)
-
     cv2.waitKey(0)
     cv2.destroyAllWindows()
