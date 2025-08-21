41aea1c4a0abf2a69658df5f33897cac6d3fe149
shinjaehyeon <pumpkinalpha996@gmail.com>
2025-08-21 09:44:48 +0900

    write pseudo code

---
+++ src/prototypes/main.py
@@ -0,0 +1,32 @@
+from ultralytics import YOLO
+import os, cv2
+import matplotlib.pyplot as plt
+
+#configs
+'''
+load video, model
+setting var
+'''
+
+#functions
+'''
+def
+'''
+
+#codes
+'''
+if __name__ == '__main__':
+    call_images
+        if no image:
+            break
+    
+    call yolo model for detection
+        detect crosswalk, car, pedestrian
+        if traffic light == green:
+            if pedestrian is True:
+                check distance between car and crosswalk:
+                    illegal
+            else:
+                legal
+
+'''
